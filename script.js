534a1d8fe2e1cd93303da3e74ba0ac09e50ee351
rosey824 <emlou.rose13@gmail.com>
2024-06-25 20:06:05 -0500

    js button

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function toggleMobileMenu(){
     document.getElementById("menu").classList.toggle
     ("active");
 }
+
+document.getElementById('resetButton').addEventListener('click', function() {
+    map.setZoom(7);
+    map.setCenter(iowa);
+});
