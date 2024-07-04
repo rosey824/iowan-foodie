625266d4f6457fa88000b4666331490656d590e4
rosey824 <emlou.rose13@gmail.com>
2024-07-04 17:17:25 -0500

    const

--- script.js
+++ script.js
@@ -102,7 +102,7 @@ function initMap() {
 
     // Add markers for each place on map
     places.forEach(function(place) {
-        var marker = new google.maps.marker.AdvancedMarkerElement({
+        const marker = new google.maps.marker.AdvancedMarkerElement({
             position: { lat: place.lat, lng: place.lng },
             map: map,
             title: place.name
