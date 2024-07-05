a9ba4884fa1d371b270de475c7bea20b82c8c9e9
rosey824 <emlou.rose13@gmail.com>
2024-07-05 15:51:52 -0500

    map priority

--- script.js
+++ script.js
@@ -5,7 +5,48 @@ function toggleMobileMenu() {
 let map;
 const iowa = { lat: 41.878, lng: -93.097 };
 
-// List of places in Iowa
+// Initialize and add the map
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 7,
+        center: iowa,
+    });
+
+    //Custom marker icon URL
+    const customIcon = {
+        url: './imgs/custom-icon.png', // Relative path to your custom icon
+        scaledSize: new google.maps.Size(60, 60), // Size of the icon
+        origin: new google.maps.Point(0, 0), // Origin of the icon (0, 0)
+        anchor: new google.maps.Point(30, 60) // Anchor of the icon (base of the icon)
+    };
+
+    // Add markers for each place on map and create legend/list
+    const markers= [];
+    const legendDiv = document.getElementById('legend');
+    places.forEach((place, index) => {
+        const marker = new google.maps.Marker({
+            position: { lat: place.lat, lng: place.lng },
+            map: map,
+            title: place.name,
+            icon: customIcon
+        });
+
+    // Add an info window for each marker
+    const infoWindow = new google.maps.InfoWindow({
+        content: `<div>
+                    <h2><strong>${place.name}</strong></h2>
+                    <p>${place.address}</p>
+                    <a href="${place.googleMapLink}" target="_blank">View on Google Maps</a>
+            </div>`
+        });
+
+        marker.addListener('click', function() {
+            infoWindow.open(map, marker);
+            map.setZoom(13);  // Set zoom level
+            map.setCenter(marker.getPosition());
+        });
+
+        // List of places in Iowa
 const places = [
     {
         name: "The Dandy Lion",
@@ -161,49 +202,7 @@ const places = [
         address:"West Des Moines, IA",
         googleMapLink:"https://maps.app.goo.gl/jBBT82FaGMhH2ew46"
     },
-
 ];
-
-// Initialize and add the map
-function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 7,
-        center: iowa,
-    });
-
-    //Custom marker icon URL
-    const customIcon = {
-        url: './imgs/custom-icon.png', // Relative path to your custom icon
-        scaledSize: new google.maps.Size(60, 60), // Size of the icon
-        origin: new google.maps.Point(0, 0), // Origin of the icon (0, 0)
-        anchor: new google.maps.Point(30, 60) // Anchor of the icon (base of the icon)
-    };
-
-    // Add markers for each place on map and create legend/list
-    const markers= [];
-    const legendDiv = document.getElementById('legend');
-    places.forEach((place, index) => {
-        const marker = new google.maps.Marker({
-            position: { lat: place.lat, lng: place.lng },
-            map: map,
-            title: place.name,
-            icon: customIcon
-        });
-
-    // Add an info window for each marker
-    const infoWindow = new google.maps.InfoWindow({
-        content: `<div>
-                    <h2><strong>${place.name}</strong></h2>
-                    <p>${place.address}</p>
-                    <a href="${place.googleMapLink}" target="_blank">View on Google Maps</a>
-            </div>`
-        });
-
-        marker.addListener('click', function() {
-            infoWindow.open(map, marker);
-            map.setZoom(13);  // Set zoom level
-            map.setCenter(marker.getPosition());
-        });
 
         //Create entry for legend
         const listItem = document.createElement('div');
